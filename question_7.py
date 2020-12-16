bf26dc02e1345ab3b56ca51b77dc54b9c8e02801
melanga <dissanayakedmmb@gmail.com>
2020-12-16 19:45:26 +0530

    Create function to predict labeles using X_test data set

---
+++ question_7.py
@@ -0,0 +1,8 @@
+import question_5
+import question_4
+
+
+def predict_class_labels():
+    X_train, X_test, y_train, y_test = question_4.create_train_test_split()
+    knn = question_5.create_classifier()
+    return knn.predict(X_test)
