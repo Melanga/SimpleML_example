487cf85b359b4e64382a6bd914f792743984b8e3
melanga <dissanayakedmmb@gmail.com>
2020-12-27 17:18:31 +0530

    Create function to find the accuracy of the classifire

--- question_8.py
+++ question_8.py
@@ -4,6 +4,7 @@ import question_6
 
 
 def find_score():
+    #  find the score of the classifier using training data sets
     X_train, X_test, y_train, y_test = question_4.create_train_test_split()
     knn = question_5.create_classifier()
     score = knn.score(X_test, y_test)
